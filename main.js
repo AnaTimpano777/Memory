a55aefe6875f9e412c130dc89bdd608f72ddae56
AnaTimpano777 <632419@smcdsb.on.ca>
2018-04-24 20:35:27 -0400

    Cards, Shuffle, Match
    Added shuffle code to make the cards shuffle. Added an Array. Added code to see if the cards match one another.

--- main.js
+++ main.js
@@ -1,9 +1,42 @@
 $(document).ready(function() {
     $('img').on({
     'click': function(){
-        $(this).attr('src','images/ace-spades.png');
+        $(this).attr('src',$(this).data('card'));
+        $(this).addClass('flipped');
+        if($('.flipped').length > 1) {
+            if($('.flipped').eq(0).data('card') == $('.flipped').eq(1).data('card')) {
+                alert('You have matched the cards you lucky duck!');   
+                //remove from field
+            }
+            else {
+                alert('You clicked the wrong one.... Click the right one next time!');    
+                //flip back over
+             }
+        }
     }
 });
 });
+    
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+//
+
 
 
